a089f04d06a216a99e9fd9a6907d38f06985be95
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-05-21 11:51:04 +0100

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Title</h1>
+      <div className="container">
+        <div className="module">
+          <h3>Module</h3>
+          <p>module description</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
