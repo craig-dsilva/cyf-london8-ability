2c8b5e387d4d25e15834e2e5c83cdd65b1039d20
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-05-21 12:09:03 +0100

    structure complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p>&copy; 2022 Created by:</p>
+      <ul>
+        <li>Kate</li>
+        <li>Ali</li>
+        <li>Saf</li>
+        <li>Delroy</li>
+        <li>Alamin</li>
+        <li>Craig</li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main>
+      <h2>Title</h2>
+      <h3>Objectives</h3>
+      <ul>
+        <li></li>
+      </ul>
+      <h3>Coursework</h3>
+      <ul>
+        <li></li>
+      </ul>
+    </main>
+  );
 };
 
 export default Main;
